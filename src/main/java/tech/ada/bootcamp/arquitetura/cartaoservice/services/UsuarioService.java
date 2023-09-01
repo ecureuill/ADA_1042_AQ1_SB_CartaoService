e7cad3b2f1161d1f838c4dc9de5ea461aeb2c052
Camilla <logikasciuro@gmail.com>
2023-09-01 19:52:33 +0000

    🦺 (UsuarioService) Validate if dependent is  already registered

--- src/main/java/tech/ada/bootcamp/arquitetura/cartaoservice/services/UsuarioService.java
+++ src/main/java/tech/ada/bootcamp/arquitetura/cartaoservice/services/UsuarioService.java
@@ -3,7 +3,6 @@ package tech.ada.bootcamp.arquitetura.cartaoservice.services;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -12,19 +11,24 @@ import tech.ada.bootcamp.arquitetura.cartaoservice.entities.Cartao;
 import tech.ada.bootcamp.arquitetura.cartaoservice.entities.Dependente;
 import tech.ada.bootcamp.arquitetura.cartaoservice.entities.Endereco;
 import tech.ada.bootcamp.arquitetura.cartaoservice.entities.Usuario;
+import tech.ada.bootcamp.arquitetura.cartaoservice.exception.DependenteExistenteException;
+import tech.ada.bootcamp.arquitetura.cartaoservice.exception.UsuarioExistenteException;
+import tech.ada.bootcamp.arquitetura.cartaoservice.payloads.TipoCartao;
 import tech.ada.bootcamp.arquitetura.cartaoservice.payloads.request.CadastroUsuarioRequest;
+import tech.ada.bootcamp.arquitetura.cartaoservice.payloads.request.DependenteRequest;
 import tech.ada.bootcamp.arquitetura.cartaoservice.payloads.response.CadastroUsuarioResponse;
+import tech.ada.bootcamp.arquitetura.cartaoservice.repositories.DependenteRepository;
 import tech.ada.bootcamp.arquitetura.cartaoservice.repositories.UsuarioRepository;
-import tech.ada.bootcamp.arquitetura.exception.UsuarioExistenteException;
 
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final DependenteRepository dependenteRepository;
     private final CriarNovoCartaoService cartaoService;
 
-    public CadastroUsuarioResponse cadastrar(CadastroUsuarioRequest cadastroUsuarioRequest) throws UsuarioExistenteException {
+    public CadastroUsuarioResponse cadastrar(CadastroUsuarioRequest cadastroUsuarioRequest) throws UsuarioExistenteException, DependenteExistenteException {
         
         if(usuarioRepository.existsById(cadastroUsuarioRequest.getIdentificador())){
             throw new UsuarioExistenteException();
@@ -45,15 +49,33 @@ public class UsuarioService {
 
     }
 
-    private List<Dependente> criarDependentes(CadastroUsuarioRequest cadastroUsuarioRequest, Usuario usuario) {
-        return cadastroUsuarioRequest.getDependentes().stream().map(dependente -> 
-            {
-                Cartao cartaoAdicional = cartaoService.cadastrarAdicional(dependente.nome(), cadastroUsuarioRequest.getTipoCartao(), usuario);
-                return new Dependente(null, dependente.cpf(), dependente.nome(), usuario, cartaoAdicional, LocalDateTime.now());            
-            }
-        ).collect(Collectors.toList());
+    private List<Dependente> criarDependentes(CadastroUsuarioRequest cadastroUsuarioRequest, Usuario usuario) throws DependenteExistenteException{
+    
+        List<Dependente> dependentes = new ArrayList<Dependente>();
+        for(DependenteRequest dependenteRequest : cadastroUsuarioRequest.getDependentes()){
+            validarDependente(dependenteRequest);
+            Dependente dependente = criarDependente(dependenteRequest, usuario, cadastroUsuarioRequest.getTipoCartao());
+            dependentes.add(dependente);
+        }
+
+        return dependentes;
     }
 
+    private Dependente criarDependente(DependenteRequest dependenteRequest, Usuario usuario,
+            TipoCartao tipoCartao) {
+        Cartao cartaoAdicional = cartaoService.cadastrarAdicional(dependenteRequest.nome(), tipoCartao, usuario);
+        return new Dependente(null, dependenteRequest.cpf(), dependenteRequest.nome(), usuario, cartaoAdicional, LocalDateTime.now());
+    }
+
+    private void validarDependente(DependenteRequest dependenteRequest) throws DependenteExistenteException {
+        //TODO como testar dependentes salvos durante a transaction?
+        if (dependenteRepository.existsByCpf(dependenteRequest.cpf())) {
+            throw new DependenteExistenteException(dependenteRequest.cpf());
+        }
+
+        //Check if list has unique values
+        
+    }
     private Usuario criarUsuario(CadastroUsuarioRequest cadastroUsuarioRequest) {
         Endereco endereco = new Endereco(cadastroUsuarioRequest.getEnderecoRequest());
         
