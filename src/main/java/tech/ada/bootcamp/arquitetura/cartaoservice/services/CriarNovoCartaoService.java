13b178b7d2cba355ddd17010e4a3430d69696e91
Camilla <logikasciuro@gmail.com>
2023-09-01 15:49:06 +0000

    ♻️ (CriarNovoCartaoService) refactored

--- src/main/java/tech/ada/bootcamp/arquitetura/cartaoservice/services/CriarNovoCartaoService.java
+++ src/main/java/tech/ada/bootcamp/arquitetura/cartaoservice/services/CriarNovoCartaoService.java
@@ -19,52 +19,36 @@ import tech.ada.bootcamp.arquitetura.cartaoservice.repositories.CartaoRepository
 @RequiredArgsConstructor
 public class CriarNovoCartaoService {
     private final CartaoRepository cartaoRepository;
-    private static Random random;
     
-    public Cartao execute(TipoCartao tipoCartao, Usuario usuario){
-        LocalDate dataAtual = LocalDate.now();
-        Cartao cartao = new Cartao();
+    public Cartao execute(TipoCartao tipoCartao, Usuario usuario) {
+        Cartao cartao = createCartao(tipoCartao, usuario, false);
+        return cartaoRepository.save(cartao);
+    }
 
+    public Cartao cadastrarAdicional(String nome, TipoCartao tipoCartao, Usuario usuario) {
+        Cartao cartao = createCartao(tipoCartao, usuario, true);
+        cartao.setNomeTitular(nome);
+        return cartaoRepository.save(cartao);
+    }
+
+    private Cartao createCartao(TipoCartao tipoCartao, Usuario usuario, boolean isDependente) {
+        Cartao cartao = new Cartao();
+        cartao.setDependente(isDependente);
         cartao.setTipoCartao(tipoCartao);
         cartao.setUsuario(usuario);
         cartao.setIdContaBanco(UUID.randomUUID().toString());
         cartao.setNomeTitular(usuario.getNome());
-        cartao.setVencimentoCartao(dataAtual.plusYears(5));
-        cartao.setCodigoSeguranca(gerarNumeroAleatorio(3));
-        cartao.setNumeroCartao(gerarNumeroAleatorio(12));
-        cartao.setCreatedAt(LocalDateTime.now());
-
-        return cartaoRepository.save(cartao);
-    }
-
-    private String gerarNumeroAleatorio(int length) {
-
-        final int tamanho = length<=0?1:length;
-        IntStream stream =  getRandom().ints(tamanho,0,9);
-        StringBuilder builder = new StringBuilder();
-        stream.forEachOrdered(builder::append);
-        return builder.toString();
-    }
-
-    private static Random getRandom(){
-        if(Objects.isNull(random)){
-            random = new Random();
-        }
-        return random;
-    }
-
-    public Cartao cadastrarAdicional(String nome, TipoCartao tipoCartao, Usuario usuario) {
-        Cartao cartao = new Cartao();
-        cartao.setDependente(true);
-        cartao.setTipoCartao(tipoCartao);
-        cartao.setUsuario(usuario);
-        cartao.setIdContaBanco(UUID.randomUUID().toString());
-        cartao.setNomeTitular(nome);
         cartao.setVencimentoCartao(LocalDate.now().plusYears(5));
         cartao.setCodigoSeguranca(gerarNumeroAleatorio(3));
         cartao.setNumeroCartao(gerarNumeroAleatorio(12));
         cartao.setCreatedAt(LocalDateTime.now());
+        return cartao;
+    }
 
-        return cartaoRepository.save(cartao);
+    private String gerarNumeroAleatorio(int length) {
+        IntStream stream = new Random().ints(length, 0, 9);
+        StringBuilder builder = new StringBuilder();
+        stream.forEachOrdered(builder::append);
+        return builder.toString();
     }
 }
